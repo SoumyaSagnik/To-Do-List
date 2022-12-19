1c4b6c1c287aa37423c3411ed000d31134726ddc
Soumya Sagnik Khanda <soumyasagnik@gmail.com>
2022-12-20 04:22:39 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,17 +1,21 @@
 const search = document.querySelector('.search input');
 const addButton = document.querySelector('.search button');
 const listContainer = document.querySelector('.lists');
+const landingMess = document.querySelector('.landing-message');
 var deleteButton;
+var taskItem;
+var tc;
 
 
 search.addEventListener('keyup', (e) => {
     if (search.value === '')
         return;
     if (e.keyCode === 13)
-    addButton.click();
+        addButton.click();
 });
 
 addButton.addEventListener('click', () => {
+    landingMess.remove();
     var task = search.value;
     if (task === '')
         return;
@@ -27,7 +31,21 @@ addButton.addEventListener('click', () => {
     deleteButton = document.querySelectorAll('.lists button');
     deleteButton.forEach(button => {
         button.addEventListener('click', () => {
-            button.parentElement.remove();
+            gsap.to(button.parentNode, { duration: .01, boxShadow: 'none' });
+            gsap.to(button.parentNode, { duration: 1, opacity: 0, ease: 'power' });
+            gsap.to(button.parentNode, { duration: 1, y: '500%', ease: 'power' });
+            gsap.to(button.parentNode, { duration: .3, rotation: 10, ease: 'power' });
+            setTimeout(function () {
+                button.parentElement.remove();
+            }, 700);
         });
+    })
+    taskItem = document.querySelectorAll('.container .lists .item h4');
+    taskItem.forEach((btn) => {
+        btn.addEventListener('click', () => {
+            btn.style.textDecoration = "line-through";
+            btn.style.textDecorationColor = "green";
+            btn.style.opacity = ".7";
+        })
     })
 });
